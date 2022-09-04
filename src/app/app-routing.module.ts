d818e4de1ae6896c60c577d68542ab89a043adb8
Denis Halupczok <denis.halupczok.external@atos.net>
2022-09-04 14:14:47 +0200

    minor improvments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesListComponent },
+  {
+    path: 'recipes',
+    component: RecipesListComponent,
+  },
+  { path: 'recipe', component: RecipeComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RecipeMiniatureComponent } from './components/recipe-miniature/recipe-miniature.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [AppComponent, RecipesListComponent, NotFoundComponent, RecipeMiniatureComponent],
+  declarations: [
+    AppComponent,
+    RecipesListComponent,
+    NotFoundComponent,
+    RecipeMiniatureComponent,
+    RecipeComponent,
+    CarouselComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
